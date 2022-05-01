355d6549cf931ea9839feacdb0b805a7ba2200e9
Bachtobji95 <87646401+Bachtobji95@users.noreply.github.com>
2022-05-01 23:31:49 +0200

    Update docteur.java

--- docteur.java
+++ docteur.java
@@ -1,14 +1,18 @@
 
 public class docteur {
 
-	String nom, prenom, date, num_tel, password, email;
+	String nom, prenom, date, num_tel, password, email, password2;
 	
-	docteur(String nom, String prenom, String date, String num_tel, String password, String email){
+	docteur(String nom, String prenom, String date, String num_tel, String password, String email, String password2){
 		this.nom = nom;
 		this.prenom = prenom;
 		this.password =  password;
 		this.date = date;
 		this.num_tel = num_tel;
 		this.email = email;
+		this.password2= password2;
 	}
+	
+	
+	
 }
