4b6d9dfa4be1fbdbe622b725367143188b9777ce
Bachtobji95 <87646401+Bachtobji95@users.noreply.github.com>
2022-05-01 16:05:01 +0200

    Create class patient.java

---
+++ class patient.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner;
+public class patient {
+	
+	String nom, prenom, date, num_tel, password, email;
+	
+	
+	patient(String nom, String prenom, String date, String num_tel, String password, String email){
+		
+		this.nom = nom;
+		this.prenom = prenom;
+		this.password =  password;
+		this.date = date;
+		this.num_tel = num_tel;
+		this.email = email;
+	}
+
+}
