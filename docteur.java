386c70e698031606a738e12fc9e9671a760e3f9a
Bachtobji95 <87646401+Bachtobji95@users.noreply.github.com>
2022-05-01 16:06:40 +0200

    Create docteur.java

---
+++ docteur.java
@@ -0,0 +1,14 @@
+
+public class docteur {
+
+	String nom, prenom, date, num_tel, password, email;
+	
+	docteur(String nom, String prenom, String date, String num_tel, String password, String email){
+		this.nom = nom;
+		this.prenom = prenom;
+		this.password =  password;
+		this.date = date;
+		this.num_tel = num_tel;
+		this.email = email;
+	}
+}
